1c40f1a0e6fb1030183202908d6e5739163e7d26
Marvin Flm <72012603+MarvinFlm@users.noreply.github.com>
2024-07-16 14:39:58 +0200

    feat: Initialization of the package edgar

---
+++ lib/colors.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+
+/// Define the color of the application
+class AppColors {
+  /// White color
+  static const white = Color(0xFFFFFFFF);
+
+  /// Black color
+  static const black = Color(0xFF000000);
+
+  /// Primary color
+  /// Blue.50 color
+  static const blue50 = Color.fromRGBO(241, 247, 253, 1);
+
+  /// Blue.100 color
+  static const blue100 = Color.fromRGBO(223, 236, 250, 1);
+
+  /// Blue.200 color
+  static const blue200 = Color.fromRGBO(198, 222, 247, 1);
+
+  /// Blue.300 color
+  static const blue300 = Color.fromRGBO(160, 201, 240, 1);
+
+  /// Blue.400 color
+  static const blue400 = Color.fromRGBO(114, 172, 232, 1);
+
+  /// Blue.500 color
+  static const blue500 = Color.fromRGBO(81, 141, 224, 1);
+
+  /// Blue.600 color
+  static const blue600 = Color.fromRGBO(60, 115, 218, 1);
+
+  /// Blue.700 color
+  static const blue700 = Color.fromRGBO(51, 95, 194, 1);
+
+  /// Blue.800 color
+  static const blue800 = Color.fromRGBO(46, 76, 154, 1);
+
+  /// Blue.900 color
+  static const blue900 = Color.fromRGBO(43, 68, 125, 1);
+
+  /// Blue.950 color
+  static const blue950 = Color.fromRGBO(30, 43, 77, 1);
+
+  /// Grey colors
+  /// Grey.50 color
+  static const grey50 = Color.fromRGBO(246, 246, 249, 1);
+
+  /// Grey.100 color
+  static const grey100 = Color.fromRGBO(236, 237, 242, 1);
+
+  /// Grey.200 color
+  static const grey200 = Color.fromRGBO(213, 215, 226, 1);
+
+  /// Grey.300 color
+  static const grey300 = Color.fromRGBO(176, 180, 201, 1);
+
+  /// Grey.400 color
+  static const grey400 = Color.fromRGBO(133, 139, 171, 1);
+
+  /// Grey.500 color
+  static const grey500 = Color.fromRGBO(97, 104, 139, 1);
+
+  /// Grey.600 color
+  static const grey600 = Color.fromRGBO(81, 87, 120, 1);
+
+  /// Grey.700 color
+  static const grey700 = Color.fromRGBO(66, 70, 98, 1);
+
+  /// Grey.800 color
+  static const grey800 = Color.fromRGBO(57, 60, 83, 1);
+
+  /// Grey.900 color
+  static const grey900 = Color.fromRGBO(51, 54, 71, 1);
+
+  /// Grey.950 color
+  static const grey950 = Color.fromRGBO(34, 35, 47, 1);
+
+  /// Green colors
+  /// Green.50 color
+  static const green50 = Color.fromRGBO(242, 250, 235, 1);
+
+  /// Green.100 color
+  static const green100 = Color.fromRGBO(223, 244, 211, 1);
+
+  /// Green.200 color
+  static const green200 = Color.fromRGBO(195, 234, 172, 1);
+
+  /// Green.300 color
+  static const green300 = Color.fromRGBO(156, 219, 123, 1);
+
+  /// Green.400 color
+  static const green400 = Color.fromRGBO(121, 202, 81, 1);
+
+  /// Green.500 color
+  static const green500 = Color.fromRGBO(90, 175, 51, 1);
+
+  /// Green.600 color
+  static const green600 = Color.fromRGBO(67, 139, 37, 1);
+
+  /// Green.700 color
+  static const green700 = Color.fromRGBO(53, 107, 32, 1);
+
+  /// Green.800 color
+  static const green800 = Color.fromRGBO(45, 85, 31, 1);
+
+  /// Green.900 color
+  static const green900 = Color.fromRGBO(41, 73, 30, 1);
+
+  /// Green.950 color
+  static const green950 = Color.fromRGBO(18, 39, 12, 1);
+
+  /// Red colors
+  /// Red.100 color
+  static const red100 = Color.fromRGBO(255, 224, 224, 1);
+
+  /// Red.200 color
+  static const red200 = Color.fromRGBO(255, 199, 199, 1);
+
+  /// Red.300 color
+  static const red300 = Color.fromRGBO(255, 159, 159, 1);
+
+  /// Red.400 color
+  static const red400 = Color.fromRGBO(255, 104, 104, 1);
+
+  /// Red.500 color
+  static const red500 = Color.fromRGBO(250, 57, 57, 1);
+
+  /// Red.600 color
+  static const red600 = Color.fromRGBO(218, 22, 22, 1);
+
+  /// Red.700 color
+  static const red700 = Color.fromRGBO(195, 18, 18, 1);
+
+  /// Red.800 color
+  static const red800 = Color.fromRGBO(161, 19, 19, 1);
+
+  /// Red.900 color
+  static const red900 = Color.fromRGBO(133, 23, 23, 1);
+
+  /// Orange colors
+  /// Orange.100 color
+  static const orange100 = Color.fromRGBO(254, 236, 214, 1);
+
+  /// Orange.200 color
+  static const orange200 = Color.fromRGBO(251, 213, 173, 1);
+
+  /// Orange.300 color
+  static const orange300 = Color.fromRGBO(249, 183, 120, 1);
+
+  /// Orange.400 color
+  static const orange400 = Color.fromRGBO(245, 142, 66, 1);
+
+  /// Orange.500 color
+  static const orange500 = Color.fromRGBO(242, 110, 29, 1);
+
+  /// Orange.600 color
+  static const orange600 = Color.fromRGBO(231, 86, 19, 1);
+}
---
+++ lib/src/field_custom.dart
@@ -0,0 +1,447 @@
+import 'package:edgar/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:bootstrap_icons/bootstrap_icons.dart';
+import 'package:easy_autocomplete/easy_autocomplete.dart';
+
+/// A Field who follow the color of the application
+class CustomField extends StatefulWidget {
+  /// It's the label of the application
+  final String label;
+
+  /// Icon at the end of the text field
+  final IconData? icon; // Changed from IconData to IconData?
+  /// Bool define if it a password or no to put a visibility of the text
+  final bool isPassword;
+
+  /// It's the text Keyboard type
+  final TextInputType keyboardType;
+
+  /// Fuction onchange to define what will append on change
+  final Function(String) onChanged;
+
+  /// String to define a default value
+  final String value; // Added onChanged parameter
+  /// Int to define a max width of the widget
+  final int? maxSize;
+
+  /// Define the action to do on submit
+  final TextInputAction action;
+
+  /// Take in required parameter a Label, a  fonction onchange, action.
+  const CustomField({
+    super.key,
+    required this.label,
+    this.icon,
+    this.isPassword = false,
+    this.keyboardType = TextInputType.text,
+    required this.onChanged,
+    this.value = '',
+    this.maxSize,
+    required this.action,
+  });
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _CustomFieldState createState() => _CustomFieldState();
+}
+
+class _CustomFieldState extends State<CustomField> {
+  bool _isPasswordVisible = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return AnimatedContainer(
+          // ignore: prefer_null_aware_operators
+          width: widget.maxSize != null ? widget.maxSize?.toDouble() : null,
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+          duration: const Duration(milliseconds: 200),
+          curve: Curves.easeInOut,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(color: AppColors.blue500, width: 2),
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  obscureText: widget.isPassword && !_isPasswordVisible,
+                  keyboardType: widget.keyboardType,
+                  textInputAction: widget.action,
+                  textCapitalization: TextCapitalization.sentences,
+                  initialValue: widget.value,
+                  style: const TextStyle(
+                    color: AppColors.grey950,
+                    fontFamily: 'Poppins',
+                    fontSize: 14,
+                    textBaseline: TextBaseline.ideographic,
+                  ),
+                  decoration: InputDecoration(
+                    constraints: BoxConstraints(
+                        minWidth: 0, maxWidth: constraints.maxWidth),
+                    border: InputBorder.none,
+                    isDense: true,
+                    hintText: widget.label,
+                    hintStyle: const TextStyle(
+                      color: AppColors.grey400,
+                      fontFamily: 'Poppins',
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      textBaseline: TextBaseline.ideographic,
+                    ),
+                  ),
+                  onChanged: widget.onChanged,
+                ),
+              ),
+              if (widget.icon != null)
+                Icon(widget.icon!, color: AppColors.grey950, size: 16),
+              if (widget.isPassword)
+                GestureDetector(
+                  child: Icon(
+                    _isPasswordVisible
+                        ? BootstrapIcons.eye_slash_fill
+                        : BootstrapIcons.eye_fill,
+                    color: Colors.black,
+                    size: 16,
+                  ),
+                  onTap: () {
+                    setState(() {
+                      _isPasswordVisible = !_isPasswordVisible;
+                    });
+                  },
+                ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+/// CustomFieldSearch class for a customizable search field with validation
+class CustomFieldSearch extends StatefulWidget {
+  /// It's the label of the text field
+  final String label;
+
+  /// It's the Icon at the end of it
+  final Widget icon;
+
+  /// It's the keyboard Type on click on the text field
+  final TextInputType keyboardType;
+
+  /// It's the fonction for what to do on validate
+  final Function(String) onValidate;
+
+  /// Doing only the validate
+  final bool? onlyOnValidate;
+
+  /// What to do on click on widget
+  final Function()? onOpen;
+
+  /// Required Parameter : Label, icon, keyboardType, onValidate
+  const CustomFieldSearch({
+    super.key,
+    required this.label,
+    required this.icon,
+    required this.keyboardType,
+    required this.onValidate,
+    this.onlyOnValidate = false,
+    this.onOpen,
+  });
+
+  @override
+  State<CustomFieldSearch> createState() => _CustomFieldSearchState();
+}
+
+class _CustomFieldSearchState extends State<CustomFieldSearch> {
+  final TextEditingController _controller = TextEditingController();
+  final FocusNode _focusNode =
+      FocusNode(); // Create a FocusNode for requesting focus
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return AnimatedContainer(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+          duration: const Duration(milliseconds: 200),
+          curve: Curves.easeInOut,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(color: AppColors.blue500, width: 2),
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _controller,
+                  keyboardType: widget.keyboardType,
+                  textInputAction: TextInputAction.search,
+                  textCapitalization: TextCapitalization.sentences,
+                  style: const TextStyle(
+                    color: AppColors.grey950,
+                    fontFamily: 'Poppins',
+                    fontSize: 14,
+                    textBaseline: TextBaseline.ideographic,
+                  ),
+                  decoration: InputDecoration(
+                    constraints: BoxConstraints(
+                      minWidth: 0,
+                      maxWidth: constraints.maxWidth,
+                    ),
+                    border: InputBorder.none,
+                    isDense: true,
+                    hintText: widget.label,
+                    hintStyle: const TextStyle(
+                      color: AppColors.grey400,
+                      fontFamily: 'Poppins',
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      textBaseline: TextBaseline.ideographic,
+                    ),
+                  ),
+                  onTap: widget.onOpen,
+                  onTapAlwaysCalled: true,
+                  onSubmitted: (value) {
+                    widget.onValidate(value);
+                    _controller.clear();
+                  },
+                  onChanged: (value) {
+                    if (widget.onlyOnValidate == true) {
+                      return;
+                    }
+                    widget.onValidate(value);
+                  },
+                  focusNode:
+                      _focusNode, // Associate the FocusNode with the TextField
+                ),
+              ),
+              GestureDetector(
+                child: widget.icon,
+                onTap: () {
+                  widget.onValidate(_controller.text);
+                  _controller.clear();
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+/// TextField with an autocompletioon
+class CustomAutoComplete extends StatefulWidget {
+  /// The label of the widget
+  final String label;
+
+  /// The Icon at the end of the textfield
+  final IconData icon;
+
+  /// The keyboard Type
+  final TextInputType keyboardType;
+
+  /// The fonction on validate
+  final Function(String) onValidate;
+
+  /// The list of suggestion for the autocompletion
+  final List<String> suggestions; // Added for suggestions list
+  /// Required element : label, icon, keyboard type, onvalidate, suggestion
+  const CustomAutoComplete({
+    super.key,
+    required this.label,
+    required this.icon,
+    required this.keyboardType,
+    required this.onValidate,
+    required this.suggestions, // Added for suggestions list
+  });
+
+  @override
+  State<CustomAutoComplete> createState() => _CustomAutoComplete2State();
+}
+
+class _CustomAutoComplete2State extends State<CustomAutoComplete> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return AnimatedContainer(
+          padding: const EdgeInsets.symmetric(
+              horizontal: 16.0), // Adjust padding as needed
+          duration: const Duration(milliseconds: 200),
+          curve: Curves.easeInOut,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12.0),
+            border: Border.all(
+                color: AppColors.blue500,
+                width: 2.0), // Adjust border color and width if desired
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: EasyAutocomplete(
+                    controller: _controller,
+                    suggestions: widget.suggestions,
+                    textCapitalization: TextCapitalization.sentences,
+                    keyboardType: widget.keyboardType,
+                    decoration: InputDecoration(
+                      hintText: widget.label,
+                      border: InputBorder
+                          .none, // Remove border for seamless appearance
+                    ),
+                    onChanged: (value) {
+                      widget.onValidate(value);
+                    },
+                    suggestionBuilder: (data) {
+                      return Container(
+                          margin: const EdgeInsets.all(1),
+                          padding: const EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                              color: Colors.transparent,
+                              borderRadius: BorderRadius.circular(5)),
+                          child: Text(data,
+                              style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 12.0,
+                                  fontWeight: FontWeight.w500,
+                                  fontFamily: 'Poppins')));
+                    }),
+              ),
+              GestureDetector(
+                child: Icon(widget.icon, color: AppColors.grey950, size: 16.0),
+                onTap: () => widget.onValidate(_controller.text),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+/// It a custom field for shearch an element with a maxline
+class CustomFieldSearchMaxLines extends StatefulWidget {
+  /// Label of the custom field
+  final String label;
+
+  /// Icon at the end of the custom field
+  final Widget icon;
+
+  /// Keyboard type on click on the custom field
+  final TextInputType keyboardType;
+
+  /// Function who take a string in parameter to do something on validate
+  final Function(String) onValidate;
+
+  /// Bool to know if do only on validate
+  final bool? onlyOnValidate;
+
+  /// Function to do something on open
+  final Function()? onOpen;
+
+  /// The maxlines
+  final int maxLines;
+
+  /// Required : Label , icon, keyboardType, onValidate, maxLines
+  const CustomFieldSearchMaxLines({
+    super.key,
+    required this.label,
+    required this.icon,
+    required this.keyboardType,
+    required this.onValidate,
+    this.onlyOnValidate = false,
+    this.onOpen,
+    required this.maxLines,
+  });
+
+  @override
+  State<CustomFieldSearchMaxLines> createState() =>
+      _CustomFieldSearchMaxLinesState();
+}
+
+class _CustomFieldSearchMaxLinesState extends State<CustomFieldSearchMaxLines> {
+  final TextEditingController _controller = TextEditingController();
+  final FocusNode _focusNode =
+      FocusNode(); // Create a FocusNode for requesting focus
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return AnimatedContainer(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+          duration: const Duration(milliseconds: 200),
+          curve: Curves.easeInOut,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(color: AppColors.blue500, width: 2),
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _controller,
+                  maxLines: widget.maxLines,
+                  keyboardType: widget.keyboardType,
+                  textInputAction: TextInputAction.search,
+                  textCapitalization: TextCapitalization.sentences,
+                  style: const TextStyle(
+                    color: AppColors.grey950,
+                    fontFamily: 'Poppins',
+                    fontSize: 14,
+                    textBaseline: TextBaseline.ideographic,
+                  ),
+                  decoration: InputDecoration(
+                    constraints: BoxConstraints(
+                      minWidth: 0,
+                      maxWidth: constraints.maxWidth,
+                    ),
+                    border: InputBorder.none,
+                    isDense: true,
+                    hintText: widget.label,
+                    hintStyle: const TextStyle(
+                      color: AppColors.grey400,
+                      fontFamily: 'Poppins',
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      textBaseline: TextBaseline.ideographic,
+                    ),
+                  ),
+                  onTap: widget.onOpen,
+                  onTapAlwaysCalled: true,
+                  onSubmitted: (value) {
+                    widget.onValidate(value);
+                    _controller.clear();
+                  },
+                  onChanged: (value) {
+                    if (widget.onlyOnValidate == true) {
+                      return;
+                    }
+                    widget.onValidate(value);
+                  },
+                  focusNode:
+                      _focusNode, // Associate the FocusNode with the TextField
+                ),
+              ),
+              GestureDetector(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8),
+                  child: widget.icon,
+                ),
+                onTap: () {
+                  widget.onValidate(_controller.text);
+                  _controller.clear();
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
